d24b0e73a2f5eb54f074860f0db39e93cd91a0ac
Josh Gavant <4421720+joshgav@users.noreply.github.com>
2023-10-19 10:59:47 -0500

    add fixups for deployment (#2)
    Signed-off-by: Josh Gavant <joshgavant@gmail.com>

--- deploy/deploy-via-argocd.sh
+++ deploy/deploy-via-argocd.sh
@@ -17,4 +17,5 @@ if [[ $? != 0 ]]; then
 fi
 
 ensure_namespace ${app_namespace}
-kubectl apply --namespace ${argocd_namespace} argocd-application.yaml
+kubectl apply --namespace ${argocd_namespace} -f ${this_dir}/argocd-application.yaml
+
